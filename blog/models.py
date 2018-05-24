59a4670925daf583ff7ce9270368f2265d4b9dd5
himself65 <761282619@qq.com>
2018-05-24 23:14:45 +0800

    添加文章封面

--- blog/models.py
+++ blog/models.py
@@ -10,6 +10,8 @@ class Article(models.Model):
 
     title = models.CharField(max_length=50)
 
+    head_img_url = models.URLField(blank=True, null=True)
+
     content = models.TextField()
 
     created_time = models.DateField(auto_now_add=True)
@@ -18,3 +20,32 @@ class Article(models.Model):
 
     def __str__(self):
         return self.title
+
+    def __unicode__(self):
+        return self.title
+
+
+# TODO
+# 随后添加本地存储图片
+# class Image(models.Model):
+#     """图片"""
+
+#     image = models.ImageField(
+#         upload_to='pic_folder/', default='pic_folder/None/404.jpg')
+
+#     # author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+#     # title = models.CharField(max_length=100)
+
+#     # caption = models.CharField(max_length=250, blank=True, null=True)
+
+#     # source = models.ImageField(upload_to='photos')
+
+#     def __str__(self):
+#         return self.title
+
+#     def __unicode__(self):
+#         return self.title
+
+#     def get_absolute_url(self):
+#         return ('photo_detail', None, {'object_id': self.id})
